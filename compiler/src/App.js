f67f44d5b0416f7ce84f30c71bff7a4cfe9d5c12
duanmingming <geekdmm@163.com>
2019-05-05 14:42:08 +0800

    add dir

---
+++ compiler/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import * as bootstrap from "react-bootstrap";
+
+class App extends Component {
+  render() {
+    return (
+      <bootstrap.TabPane>
+        <bootstrap.Alert variant="secondary">
+          GeekDuan の 自制编译器
+        </bootstrap.Alert>
+        <bootstrap.FormGroup>
+          <bootstrap.FormControl
+            as="textarea"
+            style={{ height: 480 }}
+            placeholder="Enter your code"
+          />
+        </bootstrap.FormGroup>
+        <bootstrap.Button bsStyle="danger" style={{ width: 100 }}>
+          运行
+        </bootstrap.Button>
+      </bootstrap.TabPane>
+    );
+  }
+}
+
+export default App;
